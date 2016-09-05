2d97f0cf556695646d07736e0e66b134412baa76
Giovanni Scerra AFS <giovanni.scerra@afsi.com>
2016-09-05 12:02:21 -0400

    created redux store and cards reducer

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,44 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-console.log('Hello World');
+//State example:
+//-----------------------------
+// {
+//     cards: [{ deckId: 123, data: { front:'front text', back: 'back text'} }],
+//     decks: [{id: 123}]
+// };
+//
+//Store dispatching example:
+//-----------------------------
+// store.dispatch({
+//     type: 'ADD_CARD',
+//     data: {
+//         front: 'front text',
+//         back: 'back text'
+//     }
+// });
+
+var cardsReducer = function cardsReducer(state, action) {
+    switch (action.type) {
+        case 'ADD_CARD':
+            var newCard = Object.assign({}, action.data, {
+                score: 1,
+                id: +new Date()
+            });
+
+            return state.concat([newCard]);
+
+        default:
+            return state || [];
+    }
+};
+
+var store = Redux.createStore(Redux.combineReducers({
+    cards: cardsReducer
+}));
+
+store.subscribe(function () {
+    console.log(store.getState());
+});
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-console.log('Hello World');
+//State example:
+//-----------------------------
+// {
+//     cards: [{ deckId: 123, data: { front:'front text', back: 'back text'} }],
+//     decks: [{id: 123}]
+// };
+//
+//Store dispatching example:
+//-----------------------------
+// store.dispatch({
+//     type: 'ADD_CARD',
+//     data: {
+//         front: 'front text',
+//         back: 'back text'
+//     }
+// });
+
+const cardsReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_CARD':
+            let newCard = Object.assign({}, action.data, {
+                score: 1,
+                id: +new Date
+            });
+
+            return state.concat([newCard]);
+
+        default:
+            return state || [];
+    }
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+    cards: cardsReducer
+}));
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
